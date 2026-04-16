c062b28874f9049eacc1f44d5ef188f37167be91
Ana Halatiu <ana-maria-iulia.halatiu@student.upt.ro>
2026-04-16 20:33:42 +0300

    Week 6: Initial setup with Report struct and permission helpers

---
+++ city_manager.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <sys/stat.h>
+#include <time.h>
+
+typedef struct {
+    int report_id;              
+    char inspector_name[50];     
+    float lat, lon;              
+    char category[30];          
+    int severity;                
+    time_t timestamp;            
+    char description[112];       
+} Report;
+
+
+void get_perm_string(mode_t mode, char *str) {
+    str[0] = (mode & S_IRUSR) ? 'r' : '-';
+    str[1] = (mode & S_IWUSR) ? 'w' : '-';
+    str[2] = (mode & S_IXUSR) ? 'x' : '-';
+    str[3] = (mode & S_IRGRP) ? 'r' : '-';
+    str[4] = (mode & S_IWGRP) ? 'w' : '-';
+    str[5] = (mode & S_IXGRP) ? 'x' : '-';
+    str[6] = (mode & S_IROTH) ? 'r' : '-';
+    str[7] = (mode & S_IWOTH) ? 'w' : '-';
+    str[8] = (mode & S_IXOTH) ? 'x' : '-';
+    str[9] = '\0';
+}
+
+int main(int argc, char *argv[]) {
+    printf("City Manager Phase 1 Ready.\n");
+    return 0;
+}
